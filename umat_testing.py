a4ae229972a43de1ad506b18c881edcc047155c9
sunwhale <sunwhale@126.com>
2017-04-27 22:32:00 +0800

    1.0.8.3

--- umat_testing.py
+++ umat_testing.py
@@ -12,7 +12,8 @@ from Constants import *
 from workbench import workbench
 from compare_exp_sim import compare_exp_sim
 
-for name in ['7047']:
-    workbench(name,loading_cycles=10,copy=False)
-#    compare_exp_sim(name,2,'axial_stress','shear_stress')
-    compare_exp_sim(name,5,'axial_strain','axial_stress')
+#for name in ['7047']: #TC-IP
+for name in ['7046']: #NPR-IP
+    workbench(name,loading_cycles=100,copy=False)
+    compare_exp_sim(name,2,'axial_stress','shear_stress')
+#    compare_exp_sim(name,5,'axial_strain','axial_stress')
